3fd3e26be637fe20d47f08df6dfc4735ad22449d
ChillingJihan <69563429+jihan-chillin@users.noreply.github.com>
2022-02-05 12:10:24 +0900

    기본 프론트 환경설정

---
+++ prepare/front/pages/profile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import AppLayout from '../components/AppLayout';
+
+const Profile = () => (
+  <AppLayout>
+    <div>내 프로필</div>
+  </AppLayout>
+);
+
+export default Profile;
---
+++ prepare/front/pages/signup.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import AppLayout from '../components/AppLayout';
+
+const Signup = () => (
+  <AppLayout>
+    <div>회원가입</div>
+  </AppLayout>
+);
+
+export default Signup;
