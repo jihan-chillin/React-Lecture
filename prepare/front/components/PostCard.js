fb357031daa316efc6fe7d633deeba2d54b5e8a8
jihan-chillin <kimjihan77@naver.com>
2022-02-22 12:16:17 +0900

    포스트폼 구현 중

---
+++ prepare/front/components/PostCard.js
@@ -0,0 +1,9 @@
+const PostCard = () =>{
+    return (
+        <div>
+            요요 포스트카드
+        </div>
+    )
+}
+
+export default PostCard
--- prepare/front/pages/index.js
+++ prepare/front/pages/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
 import AppLayout from '../components/AppLayout';
+import PostForm from '../components/PostForm';
+import PostCard from '../components/PostCard';
 
 const Home = () =>{
-  const {isLoggedIn}
+  const { isLoggedIn } = useSelector((state)=> state?.user);
+  const { mainPosts } = useSelector((state) => state?.post)
   return (
     <AppLayout>
-      <PostForm/>
-      <PostCard/>
+      {isLoggedIn && <PostForm/>}
+      {/* map 돌릴 때, key값을 index대신 post.id로 쓰는 것이 CRUD관리에 용이  */}
+      {mainPosts.map((post)=> <PostCard key={post.id} post={post}/>)}
     </AppLayout>
   )
 }
