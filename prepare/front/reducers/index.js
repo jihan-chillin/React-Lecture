c49a1e8cca4ccdf17372fb2aa2f14dcf0f956b77
ChillingJihan <69563429+jihan-chillin@users.noreply.github.com>
2022-02-22 17:50:21 +0900

    merge conflict

--- prepare/front/reducers/index.js
+++ prepare/front/reducers/index.js
@@ -6,12 +6,12 @@ import post from './post'
 
 const initialState = {
     // 이걸 작성하려면 초기데이터 구조를 어느정도 잡아놓고 있어야 함.
-    user : {
-       
+    user : { 
+
     },
     post : {
-        
-    }
+
+    },
 }
 // 1. state를 바꾸고 싶을 때마다 action을 만들고,
 // 만들어진 action을 dispatch할 수 있게 된다. ===> 이 말의 뜻은..?!
@@ -19,10 +19,8 @@ const initialState = {
 //     type : 'CHANGE_NICKNAME',
 //     data : 'jihani'
 // }
-
 // 3. aync action creater
 // 
-
 // 2. 매번 state값을 바꿔줄 수도 있는 부분은
 // 함수로 뽑아내서 동적데이터 집어넣어서 처리할 수 있음!
 // action creater
@@ -33,8 +31,6 @@ const changeNickname = (data) =>{
         data,
     }
 }
-
-changeNickname('Jiphago')
 // Store.dispatch(changeNickname('쟈니쟈니'))
 
 // (이전상태, 액션) => 다음 상태
@@ -53,7 +49,7 @@ const rootReducer = combineReducers({
     },
     user,
     post,
-}  )
+})
 
 
-export default rootReducer;
+export default rootReducer; 
